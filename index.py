8c21feeeac639ee8c4eaf8f041842ff834fa894d
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-08-05 13:58:27 -0500

    Changed the data frame setup and allowed for lunches to be stored

--- index.py
+++ index.py
@@ -15,29 +15,58 @@ if not exists(scheduleDataPath):
 
 # Getting schedule data
 f = open(scheduleDataPath)
-schedules = json.load(f)
+data = json.load(f)
+schedules = data["students"]
+teacherData = data["teachers"]
 f.close()
 
 # Creating index list
 index = []
 for i in range(7):
     index.append("Period: "+str(i+1))
+index.append("A Lunch")
+index.append("B Lunch")
+index.append("C Lunch")
+
+lunches = {
+    "A": "",
+    "B": "",
+    "C": ""
+}
+
+firstTeacher = ""
 
 # Creating new data
-teachers = {}
+dataFrame = {}
 for name in schedules:
     for i in range(len(schedules[name])):
-        teacher = schedules[name][i][0]
-        period = schedules[name][i][1] - 1
+        teacher = schedules[name][i]
+        period = i
 
-        if (not teachers.__contains__(teacher)):
-            teachers[teacher] = np.full((7), "").tolist()
-        teachers[teacher][period] += name + ", "
-        
-        
+        # First Teacher
+        if firstTeacher == "":
+            firstTeacher = teacher
+
+        # Lunch
+        if period == 4:
+            if teacherData.__contains__(teacher):
+                lunch = teacherData[teacher]["Lunch"]
+                lunches[lunch] += name +", "
+            else:
+                print("Warning: No teacher data for \"" + teacher +"\"")
+
+
+        if (not dataFrame.__contains__(teacher)):
+            dataFrame[teacher] = np.full((10), "").tolist()
+        dataFrame[teacher][period] += name + ", "
+
+# Adding Lunches to dataFrame
+dataFrame[firstTeacher][7] = lunches["A"]
+dataFrame[firstTeacher][8] = lunches["B"]
+dataFrame[firstTeacher][9] = lunches["C"]
 
 # Creating dataframe to easily write to file
-df = pd.DataFrame(teachers, index=index)
+df = pd.DataFrame(dataFrame, index=index)
 
 # Writing data
 if exists(csvFilePath):
